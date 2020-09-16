9482f0a75d93c81bb4fca43e3e474a1a157dfc70
Caleb Reath <carreath@icloud.com>
2020-09-16 12:20:30 -0300

    Fixed EnvConfig Missing error allowing for production builds

--- erd-explorer/types/dotenvrc/envrc.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import ProcessEnv = NodeJS.ProcessEnv;
-
-export interface EnvConfig {}
-export declare function parse(config?: EnvConfig): ProcessEnv;
-export declare function inject(config?: EnvConfig): void;
